8304c3f5334aa063daba8eae84ab78d0bf03d460
oscarfiscal <oscarfiscalcp@gmail.com>
2022-03-05 10:34:49 -0500

    guardar fecha en la tabla peticion

---
+++ app/Http/Middleware/Peticion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Peticion as Pe;
+
+class Peticion
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Pe::create(['fecha' => now()]);
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+Route::middleware(['auth:sanctum', 'verified','peticion'])->group(function(){
     Route::resource('/productos', ProductoController::class);
     Route::get('/dashboard', function(){
         return view('dashboard');
